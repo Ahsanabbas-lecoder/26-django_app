83ece426507962bda301cf6ed63e8f74cc308c67
Ahsanabbas-lecoder <ahsanabbaskhican@gmail.com>
2025-07-03 11:00:26 +0500

    first commit

--- web/main_app/views.py
+++ web/main_app/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
-from django.contrib.auth import login,aauthenticate,logout
+from django.shortcuts import render,redirect
+# from django.contrib.auth import login,aauthenticate,logout
+from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.forms import AuthenticationForm
-from .form import Usersignup
+from .form import Usersignupform
+from django.contrib import messages
 
 # Create your views here.
 def indexview(request):
     return render(request,'index.html')
 
-def login(request):
+def loginview(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request=request, data=request.POST)
+        form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
@@ -22,12 +24,24 @@ def login(request):
     return render(request, 'login.html')
 
     # return render(request,'login.html')
-def signup(request):
+def signupview(request):
     if request.method == "POST":
-        form = Usersignup(request.POST)
+        form = Usersignupform(request.POST)
         if form.is_valid():
-            form.save()
+            User=form.save()
+            login(request,User)
+            messages.success(request,f"Your account has been created")
             return redirect('login')
         else:
-            return render(request,'signup.html',{'form':form})
-    return render(request,'signup.html')
+            messages.error(request,f"Your don't have permission")
+
+
+    else:
+            form = Usersignupform()
+            return render(request,'signin.html',{'form':form})
+    return render(request,'signin.html')
+
+def logout_view(request):
+    logout(request)
+    messages.info(request,"you have successfully logged out")
+    return redirect('login')
